cbc560f85039fcce6406e60be8b4fbc0dfccc5cc
Yazmene <yazmenet@Yazmenes-Laptop.local>
2024-06-17 22:46:20 -0400

    code is functioning

--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -1,4 +1,34 @@
 package org.launchcode.techjobs.persistent.models;
 
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
 public class Employer extends AbstractEntity {
+    @NotBlank(message = "Location is required")
+    @Size(max = 100, message = "Location must be 100 characters or less")
+    private String location;
+@OneToMany
+@JoinColumn(name = "employer_id")
+private List<Job> jobs = new ArrayList<>();
+   public Employer() {}
+    public String getLocation() {
+        return location;
+    }
+    public void setLocation(String location) {
+        this.location = location;
+    }
+    public List<Job> getJobs() {
+        return jobs;
+    }
+    public void setJobs(List<Job> jobs) {
+        this.jobs = jobs;
+    }
 }
